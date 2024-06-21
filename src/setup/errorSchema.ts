39fb4393794ab98a7ab8ff500fdbb1a1556b963c
octavio.pincha <octavio.pincha@gmail.com>
2024-06-21 03:25:36 +0000

    chore(errorSchema): add interface to error message schema zod

--- src/setup/errorSchema.ts
+++ src/setup/errorSchema.ts
@@ -1,4 +1,15 @@
-export const apiSchemaErros = {
+interface IApiSchemaErros {
+  armors: {
+    invalid_type_error: string,
+    required_error: string
+  },
+  apparition_episodes: {
+    invalid_type_error: string,
+    required_error: string
+  }
+}
+
+export const apiSchemaErros: IApiSchemaErros = {
   armors: {
     invalid_type_error: '',
     required_error: ''
