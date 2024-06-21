d72206a75960c73dd320815a577bb05b12492ed3
octavio.pincha <octavio.pincha@gmail.com>
2024-06-21 03:32:30 +0000

    chore(setHeaderOnStatic): fix type and code problem

--- src/utils/setHeaderOnStatic.ts
+++ src/utils/setHeaderOnStatic.ts
@@ -1,10 +1,12 @@
 import mime from 'mime'
 import { fileURLToPath } from 'node:url'
 import { dirname as DN } from 'node:path'
+import { Response } from 'express'
 
-const __fileName = fileURLToPath(import.meta.url)
+const URL_WITH_ESM = import.meta.url
+const __fileName = fileURLToPath(URL_WITH_ESM)
 const __dirname = DN(__fileName)
-const setHeaderOnStatic = (res: any, path: string) => res.set('content-type', mime.getType(path))
+const setHeaderOnStatic = (res: Response, path: string) => res.set('Content-Type', mime.getType(path)!)
 const staticOptions = { setHeaders: setHeaderOnStatic }
 
 export {
