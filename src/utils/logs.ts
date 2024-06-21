d72272f1424cba0882479ad86e74e069e5a805db
octavio.pincha <octavio.pincha@gmail.com>
2024-06-21 03:29:23 +0000

    chore(log): add comment and fix types in param log fn

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,1 +1,3 @@
-export const log = (...args: any): void => globalThis.console.log(args)
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/array-type */
+export const log = (message?: any, ...optionalParams: any[]): void => globalThis.console.log(message, optionalParams)
